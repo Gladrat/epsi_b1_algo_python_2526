51da2111618b2b1c1c03718dbcc2a7c0366d6ce2
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2025-09-23 11:50:18 +0200

    update

---
+++ exercice/08.adn.py
@@ -0,0 +1,12 @@
+c = "CCATTAGTTA"
+print(c)
+
+# On peut récupérer la longueur d'une string comme une liste
+print(len(c))
+
+# On peut interroger les index d'une string comme une liste
+print(c[0])
+print(c[-1])
+
+# INTERDIT de modifier les strings comme une liste
+c[0] = "Z"
