77a8717ea2b1f016bc6287e0763a7b246a803ea3
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2025-09-23 11:40:53 +0200

    update

---
+++ cours/06.fonctions.py
@@ -0,0 +1,13 @@
+prenom = "Geoffroy"
+prenom2 = "Luc"
+
+
+def dire_bonjour(p_prenom, p_prenom2, p_prenom3, p_prenom4):
+    print(f"hello! {p_prenom} et {p_prenom2} et {p_prenom3} et {p_prenom4}")
+
+
+def main():
+    dire_bonjour("Geoffroy", prenom, prenom2, "Lisa")
+
+
+main()
