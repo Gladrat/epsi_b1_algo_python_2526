ccc441b6292306b6f14f37bc2c8f3906771c29fc
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2025-09-23 10:21:22 +0200

    update

---
+++ cours/05.f-strings.py
@@ -0,0 +1,13 @@
+prenom = "Geoffroy"
+
+print("Coucou", prenom, ", comment tu vas ?")
+print("Coucou " + prenom + ", comment tu vas ?")
+
+# f-strings
+
+print(f"Coucou {prenom}, comment tu vas ?")
+
+note = 14.5
+print("Bravo Antoine, tu as eu : " + str(note) + "/20")
+print(f"Bravo, Antoine, tu as eu : {note * 5}/100")
+
