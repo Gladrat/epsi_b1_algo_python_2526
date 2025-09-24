7e0bac6ceaf4c247f81ae58aad0ae97c511ab25c
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2025-09-24 12:05:36 +0200

    update

--- exercice/09.dungeon_master.py
+++ exercice/09.dungeon_master.py
@@ -4,9 +4,32 @@ def generer_niveau():
     niveau = []
     for i in range(20):
         niveau.append(random.randint(0, 1))
-    # print(niveau)
     return niveau
 
+gagne = False
+nb_partie = 0
 
-un_niveau = generer_niveau()
-print(un_niveau)
+while gagne == False:
+    nb_partie = nb_partie + 1
+    niveau = generer_niveau()
+    print(niveau)
+    vie = 1
+    for salle in niveau:
+        if salle == 0:
+            print("Salle vide")
+        elif salle == 1:
+            print("Combat")
+
+            des = random.randint(1, 6)
+            if des <= 3:
+                vie = vie - 1
+                print(f"Une vie perdue ! (Il en reste {vie})")
+
+        if vie <= 0:
+            print("JEU PERDU !")
+    
+    if vie > 0:
+        gagne = True
+
+print("JEU GAGNE !")
+print(f"Vous avez fait : {nb_partie} parties")
